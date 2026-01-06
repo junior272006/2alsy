c0fdf00a91dd3388b104b842e1e12a697a07d918
junior272006 <junior27nguetta@gmail.com>
2026-01-06 14:48:02 +0000

    first

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,22 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter , Routes,Route} from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css';
 import { theme } from './styles/theme.ts';
 import Home from './pages/public/Home.tsx';
-import App from './App.tsx';
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <HashRouter>
-        <Routes>
-          <Route path='/' element={<App />} />
-          <Route path='/home' element={<Home />} />
-        </Routes>
-      </HashRouter>
-    </MantineProvider>
+      <BrowserRouter>
+      <Routes>
+   <Route path='/app' element={< App/>} />
+   <Route path='/' element={< Home/>} />
+    </Routes>
+    </BrowserRouter>
+     </MantineProvider>
   </StrictMode>
-);
+ 
+)
